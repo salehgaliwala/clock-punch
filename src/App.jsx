33afcbf5ac19ba14b2600ca6984b71cfe86ebc79
Saleh Galiwala <unimax.systems@gmail.com>
2026-04-13 18:32:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { History, X, Settings, Shield, Plus, Edit, Trash2, Clock, Minus } from '
 import './index.css';
 
 // CONFIG: Replace this with your deployed Apps Script URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbxi8RgNvVDDCeu2LzuUWCFRcPCswHjy6aJ7NkMN0eGzHnA9K0Pr59eY3SEaAHl7zxLR/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwCpjAA0tvsoTn8yW5QAG3vjiVIw-tTZg1ra9JHvyn_ThJqnYuJwo60I5iMO3ybxFZgUg/exec';
 
 // Safely format dates to prevent app crashes on invalid data
 const safeDateFormat = (dateString, formatStr) => {
